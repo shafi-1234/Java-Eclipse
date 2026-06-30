61485264fdb3ded8dff9cee9cabf2a31c7eebe04
Mahammad shafi <mahammadshafishaik444@gmail.com>
2026-06-30 14:38:26 +0530

    Intial Commit

---
+++ project1/src/com/basics/Crickters.java
@@ -0,0 +1,21 @@
+package com.basics;;
+
+
+public class Crickters {
+	static String teamName = " India";
+	static int countryCode = 91;
+	
+	String playerName;
+	int jersyNo;
+	int totalRuns;
+	void display() {
+		System.out.println("Team Name");
+	}
+
+	public static void main(String[] args) {
+		
+		Crickters p1 = new Crickters();
+		
+	}
+
+}
---
+++ project1/src/com/basics/Example1.java
@@ -0,0 +1,10 @@
+package com.basics;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		System.out.println("Example 1");
+
+	}
+
+}
