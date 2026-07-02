803929a46623b747d3773ccca9627de3a42a6f5b
Mahammad shafi <mahammadshafishaik444@gmail.com>
2026-07-02 10:54:36 +0530

    New Package Commit

--- project1/src/com/basics/Bike.java
+++ project1/src/com/basics/Bike.java
@@ -1,4 +1,5 @@
-package com.basics;
+package com.basics
+;
 
 public class Bike {
 
---
+++ project1/src/com/languageFundaments/Identifiers1.java
@@ -0,0 +1,43 @@
+package com.languageFundaments;
+
+public class Identifiers1 {
+
+	public static void main(String[] args) {
+//		An Identifier is a name given to a Java program element.
+//		The following are considered Identifiers:
+//		-->Package Name
+//		-->Class Name
+//		-->Interface Name
+//		-->Enum Name
+//		-->Method Name
+//		-->Constructor Name
+//		-->Variable Name
+//		-->Constant Name
+//		-->Parameter (Argument) Name
+//		-->Label Name
+		System.out.println("What is The Identifier?");
+		System.out.println("An Identifier is a name given to a Java program element.\r\n"
+				+ "		The following are considered Identifiers:\r\n"
+				+ "		-->Package Name\r\n"
+				+ "		-->Class Name\r\n"
+				+ "		-->Interface Name\r\n"
+				+ "		-->Enum Name\r\n"
+				+ "		-->Method Name\r\n"
+				+ "		-->Constructor Name\r\n"
+				+ "		-->Variable Name\r\n"
+				+ "		-->Constant Name\r\n"
+				+ "		-->Parameter (Argument) Name\r\n"
+				+ "		-->Label Name");
+		
+		System.out.println("--------------------------------------");
+		System.out.println("Rules of The Identifiers ");
+		System.out.println("1) First Character: Must start with a letter, _, or $.\n" + "2) It cannot start with a number.\n"+"3) No Keywords: Reserved words like int, class, or public are forbidden.\n"+"4) Case-Sensitive: myVar and myvar are completely different names.\n"+"4) No Spaces: Internal spaces or punctuation marks (like @ or #) are not allowed.");
+		
+		
+		
+		
+
+
+	}
+
+}
