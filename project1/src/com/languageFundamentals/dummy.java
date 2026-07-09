6fbfa9c418e99f448a6fe606a01f30a5e8569174
Mahammad shafi <mahammadshafishaik444@gmail.com>
2026-07-09 19:07:17 +0530

    Dummy Code

---
+++ project1/src/com/languageFundamentals/dummy.java
@@ -0,0 +1,11 @@
+package com.languageFundamentals;
+
+public class dummy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Dummy File");
+
+	}
+
+}
