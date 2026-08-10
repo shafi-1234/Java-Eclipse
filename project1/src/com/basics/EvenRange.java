c7753727f5e03d0780b7aa8a717a1963fad6d5ac
Mahammad shafi <mahammadshafishaik444@gmail.com>
2026-08-10 13:34:30 +0530

    new

---
+++ project1/src/com/basics/EvenRange.java
@@ -0,0 +1,17 @@
+package com.basics;
+
+public class EvenRange {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=100;i++) {
+			if(i%2==0) {
+				System.out.println(i+" is A Even Number ");
+			}
+			
+		}
+
+	}
+	
+
+}
