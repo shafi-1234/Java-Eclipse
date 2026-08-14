d9170453b0e064c56d20c2f811a4da81df256ffe
Mahammad shafi <mahammadshafishaik444@gmail.com>
2026-08-14 17:59:45 +0530

    new

---
+++ project1/src/com/dsa/Amastrong.java
@@ -0,0 +1,36 @@
+package com.dsa;
+
+import java.util.Scanner;
+
+public class Amastrong {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number : ");
+		int n =sc.nextInt();
+		
+		for(int i=1;i<=n;i++) {
+			int count=0;
+			int rem=0;
+			int sum=0;
+			int temp=i;
+		// count a number 
+		while(temp>0) {
+			temp=temp/10;
+			count++;
+		}
+		temp=i;
+		while(temp>0) {
+			rem=temp%10;
+			sum=sum+Math.powExact(rem, count);
+			temp /=10;
+		}
+		if(sum==i) {
+			System.out.println(i+" is Amastrong");
+		}
+		}
+		
+	}
+		
+
+}
