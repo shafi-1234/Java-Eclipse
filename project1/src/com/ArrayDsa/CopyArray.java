4d2e9de0c48302c6dfee189160ba4bdbb32f39cb
Mahammad shafi <mahammadshafishaik444@gmail.com>
2026-08-22 21:36:08 +0530

    New

---
+++ project1/src/com/ArrayDsa/CopyArray.java
@@ -0,0 +1,14 @@
+package com.ArrayDsa;
+
+import java.util.Arrays;
+
+public class CopyArray {
+
+	public static void main(String[] args) {
+		int arr[]= {7,2,1,3,5,4};
+		int n =arr.length;
+		int copyArray[]=  arr;
+		System.out.println(Arrays.toString(copyArray));
+	}
+
+}
