d7cb8b48a9833e5abb35864aa7b18074221f7a3e
Mahammad shafi <mahammadshafishaik444@gmail.com>
2026-08-22 21:35:25 +0530

    ok

---
+++ project1/src/com/ArrayDsa/AverageOfArray.java
@@ -0,0 +1,16 @@
+package com.ArrayDsa;
+
+public class AverageOfArray {
+
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4,5,6};
+		double avg=0;
+		int sum=0;
+		for(int i=0;i<arr.length;i++) {
+			sum=sum+arr[i];
+		}
+		avg=(double)sum/arr.length;
+		System.out.println("Average Of Array : "+avg);
+	}
+
+}
