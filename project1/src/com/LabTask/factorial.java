17a29e273e0e0db87bb3f18ddd9d17fb004865f0
Mahammad shafi <mahammadshafishaik444@gmail.com>
2026-08-30 23:22:54 +0530

    Ok

---
+++ project1/src/com/LabTask/factorial.java
@@ -0,0 +1,19 @@
+package com.LabTask;
+
+import java.util.Scanner;
+
+public class factorial {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int fact=1;
+		for(int i=n;i>=1;i--) {
+			fact =fact*i;
+		}
+		System.out.println("Factorial of A Number "+n+" : "+fact);
+		sc.close();
+
+	}
+
+}
