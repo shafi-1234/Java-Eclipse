ba70388831238339e8303467d49fbfe328f886fe
Mahammad shafi <mahammadshafishaik444@gmail.com>
2026-09-20 11:10:16 +0530

    NEW

---
+++ project1/src/com/LabTask/TryCatchException/ArthematicExe.java
@@ -0,0 +1,20 @@
+package com.LabTask.TryCatchException;
+
+import java.util.Scanner;
+
+public class ArthematicExe {
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String args[]) {
+		
+//			System.out.println("Enter a Nummber : ");
+//			int s = sc.nextInt();
+
+			System.out.println(10/2);
+			System.out.println(20/0);
+			
+		
+
+		
+	}
+
+}
