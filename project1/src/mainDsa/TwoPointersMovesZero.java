fbd39bbf5ac6f8fe8172351b703225bd448d93f9
Mahammad shafi <mahammadshafishaik444@gmail.com>
2026-09-25 22:10:27 +0530

    NEW

---
+++ project1/src/mainDsa/TwoPointersMovesZero.java
@@ -0,0 +1,21 @@
+package mainDsa;
+
+import java.util.Arrays;
+
+public class TwoPointersMovesZero {
+
+	public static void main(String[] args) {
+		int arr[]= {0,1,2,0,5,0,6};
+		int j=0;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]!=0) {
+				int temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+				j++;
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
